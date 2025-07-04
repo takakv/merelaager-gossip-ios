f85636902b3e7262ca30597b27d61f09cdcf2eb8
Taka <71265675+takakv@users.noreply.github.com>
2025-07-04 22:21:20 +0300

    Initial Commit

---
+++ Gossip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gossip
+//
+//  Created by Taaniel Kraavi on 04.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gossip/GossipApp.swift
@@ -0,0 +1,17 @@
+//
+//  GossipApp.swift
+//  Gossip
+//
+//  Created by Taaniel Kraavi on 04.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GossipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
