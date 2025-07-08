2c753bb43128f29aaf78bfedefedb662a2319e6f
Taka <71265675+takakv@users.noreply.github.com>
2025-07-09 01:00:54 +0300

    Improve settings view

--- Gossip/SettingsView.swift
+++ Gossip/SettingsView.swift
@@ -10,15 +10,48 @@ import SwiftUI
 struct SettingsView: View {
     @Environment(SessionManager.self) private var sessionManager
     
+    @State private var password = ""
+    @State private var passwordCheck = ""
+    @State private var showDeleteConfirmation = false
+    
     var body: some View {
-        Text("Sätted")
-        
-        Text(sessionManager.currentUser?.username ?? "Anonüümne")
-        
-        Button {
-            sessionManager.signOut()
-        } label: {
-            Text("Logi välja")
+        NavigationStack {
+            Form {
+                Section {
+                    VStack(alignment: .leading) {
+                        Text(sessionManager.currentUser?.username ?? "Anonüümne kasutaja")
+                        Text("Postitamine lubatud.")
+                            .font(.footnote)
+                    }
+                }
+
+                Section("Konto") {
+                    Button("Vaheta salasõna") { }
+                        .tint(.blue)
+                    Button("Kustuta konto", role: .destructive) {
+                        showDeleteConfirmation = true
+                    }
+                    .alert("Kustuta konto?", isPresented: $showDeleteConfirmation) {
+                        Button("Kustuta", role: .destructive, action: {})
+                        Button("Tühista", role: .cancel) { }
+                    } message: {
+                        Text("Koos kontoga kustutatakse ka kõik sinu postitused. Sinu kontot ega postitusi taastada ei saa.")
+                    }
+                    .tint(.blue)
+                }
+                
+                Section {
+                    Button("Logi välja") {
+                        sessionManager.signOut()
+                    }
+                    .tint(.blue)
+                } footer: {
+                        Text("Pärast rakendusest välja logimist pead sa teiste postituste nägemiseks uuesti sisse logima.")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+            }
+            .navigationTitle("Konto")
         }
     }
 }
@@ -26,4 +59,5 @@ struct SettingsView: View {
 #Preview {
     SettingsView()
         .environment(SessionManager())
+        .tint(.pink)
 }
